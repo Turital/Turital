82eb60e7456b8116432043fe9032b7824576ee5d
Turital <Smirnick21@gmail.com>
2019-12-12 12:06:30 +0300

    Hi

--- main.py
+++ main.py
@@ -4,28 +4,28 @@ import test as anime
 BoyGoW = TeleBot('982167824:AAEiCXI9z3uYKKOUVingUuaki_kA2uV2n4s')
 
 def show_zodiac_bottons_on(message):
-    keyboard = types.InlineKeyboardMakeup()
+    keyboard = types.InlineKeyboardMarkup()
 
-    for zod in z.zods:
+    for zod in anime.zods:
         key_zod = types.InlineKeyboardButton(text=zod, callback_data='zodiac')
         keyboard.add(key_zod)
 
-    BoyGoW.sand_message(message.from_user.id, 'ВЫбери свой знак зодиака, МАЛЬЧИК!', reply_makeup=keyboard)
+    BoyGoW.send_message(message.from_user.id, 'Выбери свой знак зодиака, МАЛЬЧИК!', reply_markup=keyboard)
 
 @BoyGoW.message_handler(content_types=['text'])
 def get_text_message(message):
     if message.text == r'\hello':
-        BoyGoW.send_message(message.from_user.id, 'Привет, BOY!, сечас я расскажу твой гороскоп!')
+        BoyGoW.send_message(message.from_user.id, 'Привет, МАЛЬЧИК! Сечас я расскажу твой гороскоп!')
         show_zodiac_bottons_on(message)
     elif message.text == r'\help':
-        BoyGoW.send+message(message.from_user.id, 'Напиши \\hello МАЛЬЧИК!!!')
+        BoyGoW.send_message(message.from_user.id, 'Напиши \\hello МАЛЬЧИК!!!')
     else:
         BoyGoW.send_message(message.from_user.id, 'Ты не разобрался, МАЛЬЧИК, напиши \\help')
 
 @BoyGoW.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
-    if call_data == 'zodiac':
-        msg = z.get_prediction()
-        BoyGoW.sand_message(call.message.chat.id, msg)
+    if call.data == 'zodiac':
+        msg = anime.get_prediction()
+        BoyGoW.send_message(call.message.chat.id, msg)
 
 BoyGoW.polling(none_stop=True, interval=0)
